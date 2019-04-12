699fc86f9c80561a222441a0ebe86fad8523379e
Sean Rudd <seanmrudd@me.com>
2019-04-11 20:36:53 -0400

    logic finished!!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,20 @@
 var correct = 0;
 var incorrect = 0;
-var unanswered = 5;
+var unanswered = 0;
 var timerRunning = false;
+var minusTime;
 
 function endgame() {
-    $('#timer').hide(0, '#timer');
-    $('#game').hide(0, '#game');
+    // $('#timer').hide(0, '#timer');
+    // $('#game').hide(0, '#game');
     $('#game-results').show(0, '#game-results');
     scoring1();
     scoring2();
     scoring3();
     scoring4();
     scoring5();
+    clearInterval(minusTime);
+
 }
 
 function countDown() {
@@ -21,12 +24,12 @@ function countDown() {
             $('#timer').html(count - 1);
         }
         if (count == 0) {
-            clearInterval(count);
+            clearInterval(minusTime);
             endgame();
         }
     });
     if (!timerRunning) {
-        setInterval(countDown, 1000);
+        minusTime = setInterval(countDown, 1000);
         timerRunning = true;
     }
 };
@@ -51,101 +54,101 @@ console.log(answer5Array);
 $("input[type='radio']").on("click", function () {
     var answerOne = $("input[name='answer1']:checked").val();
     console.log(answerOne);
-   answer1Array.unshift(answerOne)
+    answer1Array.unshift(answerOne)
 });
 
 function scoring1() {
     var answer1 = answer1Array[0];
     console.log(answer1Array);
-    if(answer1 === 'correct') {
+    if (answer1 === 'correct') {
         correct++;
     } else if (answer1 === 'incorrect') {
         incorrect++;
-    } else unanswered--;
-    
-$('#correct').text(": " + correct);
-$('#incorrect').text(": " + incorrect);
-$('#unanswered').text(": " + unanswered);
+    } else unanswered++;
+
+    $('#correct').text(": " + correct);
+    $('#incorrect').text(": " + incorrect);
+    $('#unanswered').text(": " + unanswered);
 }
 
 $("input[type='radio']").on("click", function () {
     var answerTwo = $("input[name='answer2']:checked").val();
     console.log(answerTwo);
-   answer2Array.unshift(answerTwo)
+    answer2Array.unshift(answerTwo)
 });
 
 function scoring2() {
     var answer2 = answer2Array[0];
     console.log(answer2Array);
-    if(answer2 === 'correct') {
+    if (answer2 === 'correct') {
         correct++;
     } else if (answer2 === 'incorrect') {
         incorrect++;
-    } else unanswered--;
-    
-$('#correct').text(": " + correct);
-$('#incorrect').text(": " + incorrect);
-$('#unanswered').text(": " + unanswered);
+    } else unanswered++;
+
+    $('#correct').text(": " + correct);
+    $('#incorrect').text(": " + incorrect);
+    $('#unanswered').text(": " + unanswered);
 }
 
 $("input[type='radio']").on("click", function () {
     var answerThree = $("input[name='answer3']:checked").val();
     console.log(answerThree);
-   answer3Array.unshift(answerThree)
+    answer3Array.unshift(answerThree)
 });
 
 function scoring3() {
     var answer3 = answer3Array[0];
     console.log(answer3Array);
-    if(answer3 === 'correct') {
+    if (answer3 === 'correct') {
         correct++;
     } else if (answer3 === 'incorrect') {
         incorrect++;
-    } else unanswered--;
-    
-$('#correct').text(": " + correct);
-$('#incorrect').text(": " + incorrect);
-$('#unanswered').text(": " + unanswered);
+    } else unanswered++;
+
+    $('#correct').text(": " + correct);
+    $('#incorrect').text(": " + incorrect);
+    $('#unanswered').text(": " + unanswered);
 }
 
 $("input[type='radio']").on("click", function () {
     var answerFour = $("input[name='answer4']:checked").val();
     console.log(answerFour);
-   answer4Array.unshift(answerFour)
+    answer4Array.unshift(answerFour)
 });
 
 function scoring4() {
     var answer4 = answer4Array[0];
     console.log(answer4Array);
-    if(answer4 === 'correct') {
+    if (answer4 === 'correct') {
         correct++;
     } else if (answer4 === 'incorrect') {
         incorrect++;
-    } else unanswered--;
-    
-$('#correct').text(": " + correct);
-$('#incorrect').text(": " + incorrect);
-$('#unanswered').text(": " + unanswered);
+    } else unanswered++;
+
+    $('#correct').text(": " + correct);
+    $('#incorrect').text(": " + incorrect);
+    $('#unanswered').text(": " + unanswered);
 }
 
 $("input[type='radio']").on("click", function () {
     var answerFive = $("input[name='answer5']:checked").val();
     console.log(answerFive);
-   answer5Array.unshift(answerFive)
+    answer5Array.unshift(answerFive)
 });
 
 function scoring5() {
     var answer5 = answer5Array[0];
     console.log(answer5Array);
-    if(answer5 === 'correct') {
+    if (answer5 === 'correct') {
         correct++;
     } else if (answer5 === 'incorrect') {
         incorrect++;
-    } else unanswered--;
-    
-$('#correct').text(": " + correct);
-$('#incorrect').text(": " + incorrect);
-$('#unanswered').text(": " + unanswered);
+    } else unanswered++;
+
+    $('#correct').text(": " + correct);
+    $('#incorrect').text(": " + incorrect);
+    $('#unanswered').text(": " + unanswered);
 }
 
 $('#finish').on('click', function () {
--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -22,57 +22,82 @@ var answeredCorrect = 0;
 var answeredIncorrect = 0;
 var unanswered = 5;
 
-var triviaObject = {
-    question1: {
-        questionOne: 'Who is John?',
-        answersOne: '<button value="correct">Answer1</button></br><button value="incorrect">Answer2</button></br><button value="incorrect">Answer3</button></br><button value="incorrect">Answer4</button>',
-        promptCorrect: 'You are correct!',
-        promptIncorrect: 'Wrong!',
-        promptUnanswered: 'Out of time!'
-    },
-    question2: {
-        questionTwo: 'What is John?',
-        answersTwo: '<button value="correct">Answer1</button></br><button value="incorrect">Answer2</button></br><button value="incorrect">Answer3</button></br><button value="incorrect">Answer4</button>',
-    },
-    question3: {
-        questionThree: 'When is John?',
-        answersThree: '<button value="correct">Answer1</button></br><button value="incorrect">Answer2</button></br><button value="incorrect">Answer3</button></br><button value="incorrect">Answer4</button>',
-    },
-    question4: {
-        questionFour: 'Where is John?',
-        answersFour: '<button value="correct">Answer1</button></br><button value="incorrect">Answer2</button></br><button value="incorrect">Answer3</button></br><button value="incorrect">Answer4</button>',
-    },
-    question5: {
-        questionFive: 'Why is John?',
-        answersFive: '<button value="correct">Answer1</button></br><button value="incorrect">Answer2</button></br><button value="incorrect">Answer3</button></br><button value="incorrect">Answer4</button>',
-    },
-}
-console.log(triviaObject)
-//Click event game starts and Create 'for loop' to display question and answers
+var questions = [];
+questions[0] = 'Who is John?';
+questions[1] = 'What is John?';
+questions[2] = 'When is John?';
+questions[3] = 'Where is John?';
+questions[4] = 'How is John?';
+
+console.log(questions);
+
+var answers = [];
+answers[0] = '<button class="question1" value="correct">Answer1</button></br><button class="question1" value="incorrect">Answer2</button></br><button class="question1" value="incorrect">Answer3</button></br><button class="question1" value="incorrect">Answer4</button>';
+answers[1] = '<button class="btn1" value="correct">Answer1</button></br><button class="btn2" value="incorrect">Answer2</button></br><button value="incorrect">Answer3</button></br><button value="incorrect">Answer4</button>';
+answers[2] = '<button class="btn1" value="correct">Answer1</button></br><button class="btn2" value="incorrect">Answer2</button></br><button value="incorrect">Answer3</button></br><button value="incorrect">Answer4</button>';
+answers[3] = '<button class="btn1" value="correct">Answer1</button></br><button class="btn2" value="incorrect">Answer2</button></br><button value="incorrect">Answer3</button></br><button value="incorrect">Answer4</button>';
+answers[4] = '<button class="btn1" value="correct">Answer1</button></br><button class="btn2" value="incorrect">Answer2</button></br><button value="incorrect">Answer3</button></br><button value="incorrect">Answer4</button>';
+
+console.log(answers);
+
+var promptCorrect = 'You are correct!';
+var promptIncorrect = 'You are wrong!';
+var promptUnanswered = 'Out of time!';
+
+// var triviaObject = {
+//     question1: {
+//         questionOne: 'Who is John?',
+//         answersOne: '<button value="correct">Answer1</button></br><button value="incorrect">Answer2</button></br><button value="incorrect">Answer3</button></br><button value="incorrect">Answer4</button>',
+//         promptCorrect: 'You are correct!',
+//         promptIncorrect: 'You are wrong!',
+//         promptUnanswered: 'Out of time!'
+//     },
+//     question2: {
+//         questionTwo: 'What is John?',
+//         answersTwo: '<button value="correct">Answer1</button></br><button value="incorrect">Answer2</button></br><button value="incorrect">Answer3</button></br><button value="incorrect">Answer4</button>',
+//     },
+//     question3: {
+//         questionThree: 'When is John?',
+//         answersThree: '<button value="correct">Answer1</button></br><button value="incorrect">Answer2</button></br><button value="incorrect">Answer3</button></br><button value="incorrect">Answer4</button>',
+//     },
+//     question4: {
+//         questionFour: 'Where is John?',
+//         answersFour: '<button value="correct">Answer1</button></br><button value="incorrect">Answer2</button></br><button value="incorrect">Answer3</button></br><button value="incorrect">Answer4</button>',
+//     },
+//     question5: {
+//         questionFive: 'Why is John?',
+//         answersFive: '<button value="correct">Answer1</button></br><button value="incorrect">Answer2</button></br><button value="incorrect">Answer3</button></br><button value="incorrect">Answer4</button>',
+//     },
+// }
+// console.log(triviaObject)
+// //Click event game starts and Create 'for loop' to display question and answers
 
 $('#game-start').on('click', function () {
     gameStart();
     $('#game-start').hide('#game-start');
 })
 
-var x = 2
-var y = 5
-var z;
-
-//Player chooses an answer
 function gameStart() {
-    $('#questions').append(triviaObject.question1.questionOne);
-    $('#answers').append(triviaObject.question1.answersOne);
+    $('#questions').append(questions[0]);
+    $('#answers').append(answers[0]);
 }
 
-$('button').click(function (){
-    console.log(x+y)
 
-    if(userGuess === correct) {
+//Player chooses an answer
+// function gameStart() {
+//     $('#questions').append(triviaObject.question1.questionOne);
+//     $('#answers').append(triviaObject.question1.answersOne);
+// }
+console.log()
+$('button').click(function () {
+    var userGuess = $('<button>').val(this);
+    console.log(userGuess);
+
+    if (userGuess === correct) {
         answeredCorrect++;
     } else if (userGuess === incorrect) {
         answeredIncorrect++;
-    }else unanswered--;
+    } else unanswered--;
 })
 //Create if statement to determine right, wrong, or unanswered
 
